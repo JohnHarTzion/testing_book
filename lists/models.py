49815064845410dc04c62a86d525ac81fa88f884
JohnHarTzion <yonatanhar@yahoo.com>
2017-01-10 17:40:41 -0500

    FT tests fixed for test staging

--- lists/models.py
+++ lists/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class List(models.Model):
-	pass
+	text = models.TextField(default = '', null = True)
 
 class Item(models.Model):
 	text = models.TextField(default = '')
