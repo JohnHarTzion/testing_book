859186ae412ba39f2573814a26e53b4b976afc0b
JohnHarTzion <yonatanhar@yahoo.com>
2017-01-11 14:40:12 -0500

    made functional_tests have individual test each class

---
+++ functional_tests/base.py
@@ -0,0 +1,27 @@
+import sys
+import time
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+
+class FunctionalTests(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super(FunctionalTests, cls).setUpClass()
+		cls.server_url = cls.live_server_url
+
+	def setUp(self):
+		self.browser = webdriver.Chrome('.././chromedriver')
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def check_for_rows_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = self.browser.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
