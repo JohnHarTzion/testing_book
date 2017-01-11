43f10d491647babd91878a169e887dfaf49a2f36
JohnHarTzion <yonatanhar@yahoo.com>
2017-01-11 18:28:11 -0500

    Enforce model validation in list view

--- lists/models.py
+++ lists/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class List(models.Model):
-	text = models.TextField(default = '', null = True)
+	text = models.TextField(default = '')
 
 class Item(models.Model):
 	text = models.TextField(default = '')
